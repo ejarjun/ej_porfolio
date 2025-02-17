57981dac396ee387aeb5dfad712e1fd63432dff1
EJ ARJUN <arjunej555@gmail.com>
2025-02-16 18:52:32 -1200

    Deploy maally

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ej_portfolio/',  //  Set this to match your repo name
+  base: '/ej_porfolio/',  //  Set this to match your repo name
 });
